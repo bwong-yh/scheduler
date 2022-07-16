6ea8510670d3e12c7d0394f17b1afa70bcd7b1c7
Billy Wong <bwong.yh@protonmail.com>
2022-07-16 09:15:52 -0600

    implement conditional rendering

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -1,16 +1,21 @@
 import React from "react";
 
 import "components/InterviewerListItem.scss";
+import className from "classnames";
 
 export default function InterviewerListItem(props) {
+  const interviewerClass = className("interviewer__item", {
+    "interviewers__item--selected": props.selected
+  });
+
   return (
-    <li className="interviewers__item">
+    <li className={interviewerClass}>
       <img
         className="interviewers__item-image"
         src="https://i.imgur.com/LpaY82x.png"
-        alt="Sylvia Palmer"
+        alt={props.name}
       />
-      Sylvia Palmer
+      {(props.selected && props.name)}
     </li>
   );
 }
