5e964c614cac468992b8ddd3c6720f200bb787b9
Billy Wong <bwong.yh@protonmail.com>
2022-07-17 11:25:39 -0600

    create Status and Error components

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -6,6 +6,8 @@ import Header from "./Header";
 import Empty from "./Empty";
 import Show from "./Show";
 import Confirm from "./Confirm";
+import Status from "./Status";
+import Error from "./Error";
 
 export default function Appointment(props) {
   return (
