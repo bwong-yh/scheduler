09fd966158d8df793ad0f15f6dba06fb882fe094
Billy Wong <bwong.yh@protonmail.com>
2022-07-22 12:33:47 -0600

    refactor functions

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -5,12 +5,18 @@ export function useVisualMode(initial) {
   const [history, setHistory] = useState([initial]);
 
   function transition(nextMode) {
-    setHistory(history => [...history, nextMode]);
+    setHistory(prev => [...prev, nextMode]);
     setMode(nextMode);
   }
 
   function back() {
-    history.length === 1 ? setMode(initial) : setMode(history.splice(-2)[0]);
+    if (history.length > 1) {
+      // need to setMode before setHistory -> unable to setHistory if current mode is the last index of history array
+      setMode(history[history.length - 2]);
+      setHistory(prev =>
+        [...prev].slice(0, -1)
+      );
+    }
   }
 
   return { mode, transition, back };
